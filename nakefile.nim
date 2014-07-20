5d681b68a891a4a66fdf702f668f0e8ad23edb9b
Johz <jonathan.frere@gmail.com>
2014-07-20 19:35:46 +0100

    Proper package structure

--- nakefile.nim
+++ nakefile.nim
@@ -2,15 +2,15 @@ import nake
 import strutils
 
 const
-    DOCS_SOURCE = "lib"
+    DOCS_FILES = ["appdirs.nim"]
     DOCS_DEST = "docs"
 
 task "clean", "Removes nimcache folders, compiled exes":
-    direshell("rm -rf */**nimcache")
-    direshell("rm -rf lib/appdirs")
+    direshell("rm -rf nimcache")
+    direshell("rm -rf appdirs")
 
 task "docs", "Adds documentation to the docs/ directory":
     direshell("rm", "-rf", DOCS_DEST & "/*")
-    for source in walk_files(DOCS_SOURCE & "/*.nim"):
-        let dest = source.change_file_ext(".html").replace(DOCS_SOURCE, DOCS_DEST)
+    for source in DOCS_FILES:
+        let dest = DOCS_DEST & "/" & source.change_file_ext(".html")
         direshell("nimrod", "doc2", "--verbosity:0", "-o:" & dest, source)
